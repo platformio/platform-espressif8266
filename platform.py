37af9a5473620652f4243e57d555249bfa6a5eb0
Ivan Kravets <me@ikravets.com>
2016-05-30 16:02:12 +0300

    Use updated PlatformBase class

--- platform.py
+++ platform.py
@@ -12,14 +12,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from platformio.managers.platform import BasePlatform
+from platformio.managers.platform import PlatformBase
 
 
-class EspressifPlatform(BasePlatform):
+class EspressifPlatform(PlatformBase):
 
     def configure_default_packages(self, variables, targets):
         if not variables.get("framework"):
             self.packages['sdk-esp8266']['optional'] = False
 
-        return BasePlatform.configure_default_packages(
+        return PlatformBase.configure_default_packages(
             self, variables, targets)
