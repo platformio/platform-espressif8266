8850d60e42f486a195af0425f209b606a94f2233
yanbe <y.yanbe@gmail.com>
2017-05-27 14:21:25 +0900

    Added esp8266-rtos-sdk framework configuration to builder

---
+++ builder/frameworks/esp8266-rtos-sdk.py
@@ -0,0 +1,48 @@
+"""
+ESP8266 RTOS SDK
+
+Latest ESP8266 SDK based on FreeRTOS http://bbs.espressif.com
+
+http://github.com/espressif/ESP_8266_SDK
+"""
+
+from os.path import isdir, join
+
+from SCons.Script import DefaultEnvironment
+
+env = DefaultEnvironment()
+platform = env.PioPlatform()
+
+FRAMEWORK_DIR = platform.get_package_dir("framework-esp8266-rtos-sdk")
+assert isdir(FRAMEWORK_DIR)
+
+env.Prepend(
+    CPPPATH=[
+        join(FRAMEWORK_DIR, "include"),
+        join(FRAMEWORK_DIR, "extra_include"),
+        join(FRAMEWORK_DIR, "driver_lib", "include"),
+        join(FRAMEWORK_DIR, "include", "espressif"),
+        join(FRAMEWORK_DIR, "include", "lwip"),
+        join(FRAMEWORK_DIR, "include", "lwip", "ipv4"),
+        join(FRAMEWORK_DIR, "include", "lwip", "ipv6"),
+        join(FRAMEWORK_DIR, "include", "nopoll"),
+        join(FRAMEWORK_DIR, "include", "spiffs"),
+        join(FRAMEWORK_DIR, "include", "ssl"),
+        join(FRAMEWORK_DIR, "include", "json"),
+        join(FRAMEWORK_DIR, "include", "openssl"),
+    ],
+
+    LIBPATH=[
+        join(FRAMEWORK_DIR, "lib")
+    ],
+
+    LIBS=[
+        "cirom", "crypto", "driver", "espconn", "espnow", "freertos", "gcc",
+        "json", "hal", "lwip", "main", "mesh", "mirom", "net80211", "nopoll",
+        "phy", "pp", "pwm", "smartconfig", "spiffs", "ssl", "wpa", "wps"
+    ]
+)
+
+env.Replace(
+    LDSCRIPT_PATH=[join(FRAMEWORK_DIR, "ld", "eagle.app.v6.ld")],
+)
