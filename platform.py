a05b8108a606fd1035333619d798ed2362ba9d52
Ivan Kravets <me@ikravets.com>
2017-11-25 20:13:58 +0200

    Drop "stage" suffix from dev/platform name

--- platform.py
+++ platform.py
@@ -15,7 +15,7 @@
 from platformio.managers.platform import PlatformBase
 
 
-class Espressif8266_stagePlatform(PlatformBase):
+class Espressif8266Platform(PlatformBase):
 
     def configure_default_packages(self, variables, targets):
         if not variables.get("pioframework"):
